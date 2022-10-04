053ce98c4d407d7fc1e00afc5e5c10360a9c2755
46184597 <weichia.su@students.mq.edu.au>
2022-10-04 18:20:44 +1100

    create registerScreen

---
+++ APP/AppScreen/RegisterScreen.js
@@ -0,0 +1,164 @@
+import React from 'react';
+import { View,StyleSheet,TouchableOpacity, Image} from 'react-native';
+import AppButton from '../Components/AppButton';
+import AppIcon from '../Components/AppIcon';
+import AppInput from '../Components/AppIput';
+import AppText from '../Components/AppText';
+import AppTextInput from '../Components/AppTextInput';
+import AppScreen from './AppScreen';
+
+function RegisterScreen(props) {
+    return (
+        <AppScreen>
+             <View style={styles.heading}>
+            
+                <TouchableOpacity >
+                
+                    <AppIcon
+                        name="chevron-left"
+                        size={50}
+                        style={styles.headingIcon}
+                    />
+                    
+                </TouchableOpacity>
+
+           
+                <View>
+                    <Image source={require("../assets/icon.png")} style={styles.logo}/>
+                    <AppText style={styles.logoText}>Connecting Families</AppText>
+                </View>
+          
+        </View>
+        <AppText style={styles.Title}>Register Form</AppText>
+        <View style={styles.inputContainer}>
+        
+            <View style={styles.inputContent}>
+            <AppText style={styles.subTitle}>User Name</AppText>
+                <AppInput
+                            placeholder="User Name"
+                            keyboardType="name"
+                            textContentType="username"
+                />
+                
+            </View>
+
+            <View style={styles.inputContent}>
+            <AppText style={styles.subTitle}>EMail</AppText>
+                <AppInput
+                            placeholder="Email"
+                            keyboardType="Email"
+                            textContentType="Email"
+                />
+                
+            </View>
+
+            <View style={styles.inputContent}>
+            <AppText style={styles.subTitle}>Password</AppText>
+                <AppInput
+                            placeholder="Password"
+                            keyboardType="Password"
+                            textContentType="Password"
+                />
+                
+            </View>
+
+            <View style={styles.inputContent}>
+            <AppText style={styles.subTitle}>Confirm Password</AppText>
+                <AppInput
+                            placeholder="confirmPassword"
+                            keyboardType="confirmPassword"
+                            textContentType="confirmPassword"
+                />
+                
+            </View>
+            
+        
+
+        </View>
+            
+        <View style={styles.buttonContainer}>
+            <AppButton style={styles.buttonText} title="Log In" />
+        </View>
+        
+
+        <View style={styles.textContainer}>
+
+            <TouchableOpacity  onPress={() => navigation.navigate("RegisterScreen") }>
+
+            <AppText style={styles.textStyle}> Already have an account ! </AppText>
+            
+            </TouchableOpacity>
+        </View>
+            
+        </AppScreen>
+        
+    );
+}
+const styles = StyleSheet.create({
+    heading:{
+        flexDirection:"row",
+        width:230,
+        marginLeft:20,
+        paddingTop:10,
+        justifyContent:'space-between',
+
+    },
+    logo:{
+        marginTop:10,
+        marginLeft:35,
+        width:80,
+        height:80,
+    },
+    logoText:{
+        fontSize:13,
+        paddingLeft:5,
+        fontStyle:'italic'
+    }, 
+    inputContainer:{
+        justifyContent:'space-between',
+        alignItems:'center',
+        marginTop:30,
+        height:400,
+
+    },
+    inputContent:{
+        height:100,
+        
+    },
+    inputStlye:{
+        marginLeft:10,
+        width:300,
+    },
+    Title:{
+        fontStyle:'italic',
+        marginTop:10,
+        marginLeft:15
+    },
+    subTitle:{
+        fontSize:18,
+    },
+    textContainer:{
+        marginTop:40,
+        alignItems:'center',
+        
+    },
+    textStyle:{
+        fontSize:18,
+        color:"#D6F8FF",
+        textTransform:'none'
+    },
+    buttonContainer:{
+        width:220,
+        marginTop:30,
+        alignItems:'flex-end',
+    },
+    buttonText:{
+        width:80,
+        height:40,
+        borderRadius:10,
+        paddingLeft:10
+        
+    }
+})
+
+export default RegisterScreen;
